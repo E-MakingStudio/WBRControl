64fc0f481e7aa73b3b18e5859caa9ef30da7a792
i-ta9600 <k.kuro960@gmail.com>
2017-03-24 16:46:08 +0900

    cppファイル内にシリアル出力用の関数を追加

--- WBRControl.ino
+++ WBRControl.ino
@@ -1,14 +1,15 @@
 #include <WBRControl.h>
 
+// ピン番号アサイン(0はセンサーを使用しないことを指定）
 #define RIGHT_MOTOR_PWM_GO      11    // 左モーターのＰＷＭ正接出力ピン(デジタル)
 #define RIGHT_MOTOR_PWM_BACK    10    // 左モーターのＰＷＭ逆接出力ピン(デジタル)
 #define LEFT_MOTOR_PWM_GO       6     // 右モーターのＰＷＭ正接出力ピン(デジタル)
 #define LEFT_MOTOR_PWM_BACK     9     // 右モーターのＰＷＭ逆接出力ピン(デジタル)
 #define READ_BATTERY            8     //　電圧測定用ピン(センサ番号仮)
-#define FRONT_RIGHT_SENSOR      2     // 前方右センサのピン番号(デジダル)
-#define FRONT_LEFT_SENSOR       3     // 前方左センサのピン番号（デジタル）
+#define FRONT_RIGHT_SENSOR      3     // 前方右センサのピン番号(デジダル)
+#define FRONT_LEFT_SENSOR       5     // 前方左センサのピン番号（デジタル）
 #define FRONT_CENTER_SENSOR     4     // 前方中央センサのピン番号（存在しない場合は省く）（デジタル）
-#define SIDE_RIGHT_SENSOR       5     // 右側面センサのピン番号（デジタル）
+#define SIDE_RIGHT_SENSOR       2     // 右側面センサのピン番号（デジタル）
 #define SIDE_LEFT_SENSOR        7     // 左側面センサのピン番号（デジタル）
 #define MAGNET_SENSOR           12    // 給電所で磁力を検知するためのセンサのピン番号(そもそもセンサが存在するのだろうか？)(センサ番号仮)
 
@@ -42,6 +43,7 @@ WBRControl wbr(
 
 void setup(){
   wbr.pinInit();
+  Serial.begin(9600);     // シリアル通信のオン
 }
   
 void loop(){
